deb610bc8a24ad6559c388a8ebfcef2c36a79068
Bruno Marques <127341401+BrunoMarques1@users.noreply.github.com>
2025-11-21 11:10:22 -0300

    +

--- TrabalhoFinal/src/Main.java
+++ TrabalhoFinal/src/Main.java
@@ -5,12 +5,19 @@ public class Main {
         //System.out.println("----------------------");
         medico1.setNome("Janaina");
         //System.out.println(medico1);
-
+        System.out.println("----------------------");
+        System.out.println("Consulta1");
         Paciente paciente1 = new Paciente(1,"Bruno");
         Data data1 = new Data(12,12,2022);
-        Consulta consulta = new Consulta(data1,medico1,paciente1,12.12);
+        Consulta consulta1 = new Consulta(data1,medico1,paciente1,12.12);
+        System.out.println(consulta1);
 
-        System.out.println(consulta);
+        System.out.println("----------------------");
+        System.out.println("Consulta 2");
+        Paciente paciente2 = new Paciente(1,"Jorge");
+        Data data2 = new Data(11,11,2021);
+        Consulta consulta2 = new Consulta(data2,medico1,paciente2,11.11);
+        System.out.println(consulta2);
 
     }
 
